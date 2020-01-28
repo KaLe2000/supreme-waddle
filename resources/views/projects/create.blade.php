cdd703a6d672225297333fb4b404cb701fe4910a
KaLe2000 <talovskiy@gmail.com>
2020-01-28 23:14:28 +0700

    init tailwindcss

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,22 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col">
-                <form action="{{ route('projects.store') }}" method="POST">
-                    @csrf
-                    <div>
-                        <label for="title">Title</label><br>
-                        <input type="text" id="title" name="title" value="{{ old('title') }}"><br>
-                        <label for="description">Description</label><br>
-                        <textarea name="description" id="description" cols="30" rows="10">
-                            {{ old('description') }}
-                        </textarea><br>
-                        <input type="submit">
-                    </div>
-                </form>
-            </div>
+    <div class="row justify-content-center">
+        <div class="col">
+            <form action="{{ route('projects.store') }}" method="POST">
+                @csrf
+                <div>
+                    <label for="title">Title</label><br>
+                    <input type="text" id="title" name="title" value="{{ old('title') }}"><br>
+                    <label for="description">Description</label><br>
+                    <textarea name="description" id="description" cols="30" rows="10">
+                        {{ old('description') }}
+                    </textarea><br>
+                    <input type="submit" value="Create Project">
+                    <a href="{{ route('projects.index') }}">Cancel</a>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
