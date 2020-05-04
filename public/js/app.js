6ce711fea04f08db99e84435405348390740e10a
Nikita Talovskiy <talovskiy@gmail.com>
2020-05-04 14:53:37 +0700

    rm dependency vulnerabilities

--- public/js/app.js
+++ public/js/app.js
@@ -49650,7 +49650,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/src/index.js):\nSyntaxError\n\n(32:3) `@apply` cannot be used with .shadow because .shadow is included in multiple rulesets.\n\n \u001b[90m 30 | \u001b[39m\n \u001b[90m 31 | \u001b[39m\u001b[33m.card\u001b[39m \u001b[33m{\u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 32 | \u001b[39m  \u001b[36m@apply\u001b[39m \u001b[33m.bg-gray-700\u001b[39m \u001b[33m.p-5\u001b[39m \u001b[33m.rounded\u001b[39m \u001b[33m.shadow\u001b[39m\u001b[33m;\u001b[39m\n \u001b[90m    | \u001b[39m  \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 33 | \u001b[39m\u001b[33m}\u001b[39m\n \u001b[90m 34 | \u001b[39m\n\n    at C:\\OSPanel\\domains\\supreme-waddle\\node_modules\\webpack\\lib\\NormalModule.js:316:20\n    at C:\\OSPanel\\domains\\supreme-waddle\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\OSPanel\\domains\\supreme-waddle\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:18\n    at context.callback (C:\\OSPanel\\domains\\supreme-waddle\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at C:\\OSPanel\\domains\\supreme-waddle\\node_modules\\postcss-loader\\src\\index.js:207:9");
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/src/index.js):\nSyntaxError\n\n(32:3) `@apply` cannot be used with .shadow because .shadow is included in multiple rulesets.\n\n \u001b[90m 30 | \u001b[39m\n \u001b[90m 31 | \u001b[39m\u001b[33m.card\u001b[39m \u001b[33m{\u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 32 | \u001b[39m  \u001b[36m@apply\u001b[39m \u001b[33m.bg-gray-700\u001b[39m \u001b[33m.p-5\u001b[39m \u001b[33m.rounded\u001b[39m \u001b[33m.shadow\u001b[39m\u001b[33m;\u001b[39m\n \u001b[90m    | \u001b[39m  \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 33 | \u001b[39m\u001b[33m}\u001b[39m\n \u001b[90m 34 | \u001b[39m\n\n    at runLoaders (/home/nikita/TestProjects/supreme-waddle/node_modules/webpack/lib/NormalModule.js:316:20)\n    at /home/nikita/TestProjects/supreme-waddle/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /home/nikita/TestProjects/supreme-waddle/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/home/nikita/TestProjects/supreme-waddle/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Promise.resolve.then.then.catch (/home/nikita/TestProjects/supreme-waddle/node_modules/postcss-loader/src/index.js:207:9)\n    at <anonymous>");
 
 /***/ }),
 
@@ -49661,8 +49661,8 @@ throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\OSPanel\domains\supreme-waddle\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\OSPanel\domains\supreme-waddle\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/nikita/TestProjects/supreme-waddle/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/nikita/TestProjects/supreme-waddle/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
